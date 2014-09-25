02302d13bcf65ba044aeafd56d978a52a1cd0718
Zafar Iqbal <ultrasine@gmail.com>
2014-09-25 17:07:09 +0300

    homepage dynamic and contact logic

---
+++ _teacms_/_config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+$_TEACMSCONFIG['APP_IMAGE_MODES']='
+	pages:fwx100x?x0x1|racx32x32x0x1|fwifx700x?x0x1|fwifx2001x?x0x1|racx226x226x0x1,
+';
---
+++ _teacms_/_stars.php
@@ -0,0 +1,21 @@
+<?php
+
+// triangleleft, triangleright, pentagon
+
+
+$__stars[]=array(0,"markers/page.png","Page",1,array("maintitle","shortdescription","longdescription","fuid","strapline"));
+
+$__stars[]=array(100,"markers/star-2.png","Text",1,array("maintitle","shortdescription","longdescription"));
+$__stars[]=array(101,"markers/star-3.png","Image",1,array("maintitle","fuid"));
+$__stars[]=array(102,"markers/star-4.png","Video",1,array("maintitle","embed"));
+$__stars[]=array(105,"markers/star-7.png","File",1,array("maintitle","fuid"));
+$__stars[]=array(112,"markers/star-12.png","Side Image",1,array("maintitle","fuid","link"));
+
+$__stars[]=array(301,"markers/square-1.png","Banner",1,array("maintitle","fuid")); // block301
+$__stars[]=array(302,"markers/square-2.png","Homepage Row",1,array("maintitle","strapline","link")); // block301
+
+$__stars[]=array(9002,"markers/diamond-1.png","Contact",1,array("maintitle"));
+$__stars[]=array(9004,"markers/diamond-1.png","Translation Memory",1,array("maintitle","embed"));
+$__stars[]=array(9006,"node.png","Fixed Navigation",1,array("maintitle"));
+
+
